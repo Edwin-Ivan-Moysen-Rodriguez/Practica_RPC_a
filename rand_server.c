4252f0be76ebd9042034ab6999fa216044dc8af6
Edwin-Ivan-Moysen-Rodriguez <148834209+Edwin-Ivan-Moysen-Rodriguez@users.noreply.github.com>
2023-10-27 21:35:09 -0600

    rand_server.c

--- rand_server.c
+++ rand_server.c
@@ -3,24 +3,28 @@
  * These are only templates and you can use them
  * as a guideline for developing your own functions.
  */
-#include "rand.h"
+#include "rand.h"/*Libreria necearia para la funcion de nuestro progrma, la 
+cual fue creada despues de compilar el archivo Makefile, contiene la llamada
+a los servicios necesarios para la funcion de nuestro programa*/
 
 void *
-inicializa_random_1_svc(long *argp, struct svc_req *rqstp)
+inicializa_random_1_svc(long *argp, struct svc_req *rqstp)//Servicio 1, usado para iniciar 
+//con la generacion de numeros aleatorios
 {
-	static char * result;
+	static char * result;//Variable usada para guardar el resultado
 
-	srandom(*argp);
+	srandom(*argp);//Generamos el numero aleatorio
 
-	return (void *) &result;
+	return (void *) &result;//Retornamos el resultado, a donde fue llamado el servicio
 }
 
 int *
-obtiene_siguiente_random_1_svc(void *argp, struct svc_req *rqstp)
+obtiene_siguiente_random_1_svc(void *argp, struct svc_req *rqstp)//Servicio 2, usado para obtener 
+//el siguiente numero aleatorio
 {
-	static int  result;
+	static int  result;//Variable en donde guardaremos el resultado obtenido
 
-	result=random();
+	result=random();//Asignamos un numero aleatorio
 
-	return &result;
+	return &result;//Retornamos el resultado, a donde fue llamado el servicio
 }
